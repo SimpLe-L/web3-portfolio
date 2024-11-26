68cbcb2a8fc43b8fc9a70a2faf889e4dd28bce02
simpLe <745768198@qq.com>
2024-11-26 15:50:31 +0800

    add server components use i18n example

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,9 @@
 import SelfIntroduction from "@/components/SelfIntroduction";
 import Technologies from "@/components/Technologies";
+// import { getDictionary } from "@/get-dictionaries";
 
 export default function Home() {
+  // const dict = await getDictionary("zh");
   return (
     <div className="flex flex-col items-center justify-center h-full gap-16">
       <SelfIntroduction />
